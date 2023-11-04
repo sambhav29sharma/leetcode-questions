7c083c1088e524bbe3d16c9819a7b369bd16b35c
sambhav29sharma <84859355+sambhav29sharma@users.noreply.github.com>
2023-11-04 22:30:24 +0530

    Time: 143 ms (75.82%), Space: 20.7 MB (54.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.swift
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.swift
@@ -1,20 +1,17 @@
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
         
-        var leftProduct = Array(repeating: 1, count: nums.count)
-        var rightProduct = Array(repeating: 1, count: nums.count)
-        var answer = Array(repeating: 0, count: nums.count)
+        var output = Array(repeating: 1, count: nums.count)
+        var right = 1
         
         for i in 1..<nums.count {
-            leftProduct[i] = leftProduct[i-1]*nums[i-1]
+            output[i] = output[i-1]*nums[i-1]
         }
-        for i in (0..<nums.count - 1).reversed(){
-            rightProduct[i] = rightProduct[i+1]*nums[i+1]
+        for i in (0..<nums.count ).reversed(){
+            output[i] *= right
+            right *= nums[i]
         }
-        for i in 0..<nums.count {
-            answer[i] = leftProduct[i]*rightProduct[i]
-
-        }
-        return answer
+        
+        return output
     }
 }
