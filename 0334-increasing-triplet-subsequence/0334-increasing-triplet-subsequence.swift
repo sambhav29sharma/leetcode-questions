4da61a4cfc1f327876513f475df270edc081cbc9
sambhav29sharma <84859355+sambhav29sharma@users.noreply.github.com>
2023-11-05 23:26:46 +0530

    Time: 668 ms (65.54%), Space: 32 MB (14.86%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.swift
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.swift
@@ -1,21 +1,35 @@
 class Solution {
     func increasingTriplet(_ nums: [Int]) -> Bool {
         
-        guard nums.count >= 3 else {return false}
+        // guard nums.count >= 3 else {return false}
 
-        var (small,mid) = (Int.max,Int.max)
+        // var (small,mid) = (Int.max,Int.max)
+
+        // for n in nums {
+        //     if n <= small {
+        //         small = n
+        //     }
+        //     else if n <= mid {
+        //         mid = n
+        //     }
+        //     else if n > mid {
+        //         return true
+        //     }
+        // }
+        // return false 
+        // Approach 2
+        var min1 = Int.max
+        var min2 = Int.max
 
         for n in nums {
-            if n <= small {
-                small = n
-            }
-            else if n <= mid {
-                mid = n
-            }
-            else if n > mid {
+            if n > min2 {
                 return true
-            }
-        }
-        return false 
+             } else if n > min1 {
+                 min2 = min(min2, n)
+             } else {
+                 min1 = min(min1, n)
+             }
+         }
+         return false
     }
 }
