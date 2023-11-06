bdee8ba811560f112431a44c1b66792daaa0b111
sambhav29sharma <84859355+sambhav29sharma@users.noreply.github.com>
2023-11-06 23:09:38 +0530

    Time: 158 ms (24.56%), Space: 15.3 MB (11.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.swift
+++ 0283-move-zeroes/0283-move-zeroes.swift
@@ -1,14 +1,26 @@
 class Solution {
     func moveZeroes(_ nums: inout [Int]) {
+        // var count = 0
+        // var t = 0
+        // for i in 0..<nums.count {
+        //     if (nums[i] == 0){
+        //         count += 1
+        //     }else if (count > 0){
+        //         t = nums[i]
+        //         nums[i] = 0
+        //         nums[i-count] = t
+        //     }
+        // }
+        // Approach 2
         var count = 0
-        var t = 0
-        for i in 0..<nums.count {
-            if (nums[i] == 0){
+        var i = 0
+        while i < nums.count - count {
+            if nums[i] == 0 {
                 count += 1
-            }else if (count > 0){
-                t = nums[i]
-                nums[i] = 0
-                nums[i-count] = t
+                nums.append(0)
+                nums.remove(at: i)
+            }else {
+                i += 1
             }
         }
     }
